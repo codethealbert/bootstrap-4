f9e3f1211d943be873901655ab0b6a2411255820
codethealbert <ing.alberto.lopez.l27@gmail.com>
2019-05-13 09:53:40 -0500

    Creando la seccion Reserva una cita. Parte 2: Creando el selector de fecha con datepicket

--- js/mis-scripts.js
+++ js/mis-scripts.js
@@ -58,4 +58,30 @@ $(function(){
 
     // Iniciando counterUp
     $('.counter').counterUp();
+
+    // Iniciando picker.date.js
+    $('.datepicker').pickadate({
+        monthsFull: [ 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre' ],
+        monthsShort: [ 'ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic' ],
+        weekdaysFull: [ 'Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado' ],
+        weekdaysShort: [ 'dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb' ],
+        today: 'Hoy',
+        clear: 'Borrar',
+        close: 'Cerrar',
+        labelMonthNext: 'Siguiente mes',
+        labelMonthPrev: 'Mes anterior',
+        labelMonthSelect: 'Seleccione un mes',
+        labelYearSelect: 'Seleccione un año',
+        firstDay: 1,
+        format: 'dddd d !de mmmm !de yyyy',
+        formatSubmit: 'dd/mm/yyyy',
+        selectYears: true,
+        selectMonths: true,
+        min: true,
+        max: 30,
+        onStart: function(){
+            var date = new Date();
+            this.set('select', [date.getFullYear(), date.getMonth(), date.getDate()]);
+        }
+    });
 });
